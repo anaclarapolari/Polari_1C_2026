4efec492fd8b09ebc271396a5e65fccf5a224182
Ana Clara Polari <ana.polari@ingenieria.uner.edu.ar>
2026-03-18 15:29:15 -0300

    terminé ejercicio 3 del proyecto 1

--- firmware/projects/guia1_ej3/main/guia1_ej3.c
+++ firmware/projects/guia1_ej3/main/guia1_ej3.c
@@ -26,14 +26,56 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
 /*==================[macros and definitions]=================================*/
 
 /*==================[internal data definition]===============================*/
 
+struct leds
+{
+      uint8_t mode;       //ON, OFF, TOGGLE
+	uint8_t n_led;        //indica el número de led a controlar
+	uint8_t n_ciclos;     //indica la cantidad de ciclos de encendido/apagado
+	uint16_t periodo;     //indica el tiempo de cada ciclo
+} my_leds; 
+
+enum {ON, OFF, TOGGLE};
+
 /*==================[internal functions declaration]=========================*/
 
-/*==================[external functions definition]==========================*/
-void app_main(void){
-	printf("Hello world!\n");
+void parpadeo_led (struct leds*leds)
+{
+	switch (leds->mode)
+	{
+		case ON:
+			LedOn(leds->n_led);
+			break;
+		case OFF:
+			LedOff(leds->n_led);
+			break;
+		case TOGGLE:
+			for(int i=0; i<2*leds->n_ciclos; i++)
+			{
+				LedToggle(leds->n_led);
+				vTaskDelay(leds->periodo / portTICK_PERIOD_MS);
+			}
+			break;
+	}
 }
+
+/*==================[external functions definition]==========================*/
+
+void app_main(void){
+    LedsInit();
+
+    my_leds.mode = TOGGLE;
+    my_leds.n_led = LED_1;
+    my_leds.n_ciclos = 10;
+    my_leds.periodo = 500;
+
+    parpadeo_led(&my_leds);
+}
+
 /*==================[end of file]============================================*/
